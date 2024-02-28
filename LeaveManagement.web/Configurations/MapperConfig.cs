d7855542dc2ebfa6bc6fa84ebbad2022b9e59eb7
Neel <nkp8615@gmail.com>
2024-02-28 19:25:42 +0530

    Modified and beautified  Crud Screens in LeaveType. Compeltion of Leave Type Module

---
+++ LeaveManagement.web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagement.web.Data;
+using LeaveManagement.web.Models;
+
+namespace LeaveManagement.web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig() 
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
--- LeaveManagement.web/Program.cs
+++ LeaveManagement.web/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddAutoMapper(typeof(ApplicationDbContext));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
